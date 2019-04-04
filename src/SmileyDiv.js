8d46998e0b24e11f45511858e8a3c9ace991dedd
lingyun1010 <zhaolingyun1010@gmail.com>
2019-04-04 10:53:55 +0100

    Debug feedback smiley component display in Chrome

--- src/SmileyDiv.js
+++ src/SmileyDiv.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 const SmileyDiv = styled.div`
   transition: all 0.5s;
   text-align: center;
+  display: grid;
   cursor: pointer;
   ::before {
     font-size: 2.5rem;
